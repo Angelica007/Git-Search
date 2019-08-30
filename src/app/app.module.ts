01d5b1bec664bfd4f91259c2db50b0b53c640b95
Angelica007 <angelicanagahire@gmail.com>
2019-08-30 18:23:46 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
